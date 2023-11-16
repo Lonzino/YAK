4b00cba7e019c87b199705d107398d94c2947af0
KimLonzi <luveach@gmail.com>
2023-11-16 19:27:46 +0900

    thirdtry

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1, btn2, btn3, btn4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn2 = (Button)findViewById(R.id.btn2);
+        btn3 = (Button)findViewById(R.id.btn3);
+        btn4 = (Button)findViewById(R.id.btn4);
+
+
+
+        }
+}
+
+
+
---
+++ app/src/main/java/com/example/myapplication/fragment1.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+
+import android.app.Fragment;
+
+import androidx.annotation.Nullable;
+
+public class fragment1 extends Fragment {
+
+
+
+}
